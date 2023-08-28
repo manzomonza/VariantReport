e3761a1384278e04c54bf033abcb477965c60020
manzomonza <massimiliano.manzo@posteo.ch>
2023-08-28 15:19:16 +0200

    Update Documentation and change name to VariantReport

--- R/add_QC.R
+++ R/add_QC.R
@@ -1,5 +1,12 @@
-### Add ratios
 
+#' Add columns containing forward reverse strand coverage info
+#'
+#' @param snv
+#'
+#' @return
+#' @export
+#'
+#' @examples
 add_strand_ratios = function(snv){
   snv$log2_read_ratio_Alt = log2((snv$FSAF+1)/(snv$FSAR+1))
   snv$log2_read_ratio_Ref = log2((snv$FSRF+1)/(snv$FSRR+1))
@@ -10,12 +17,28 @@ add_strand_ratios = function(snv){
 
 
 
+#' Select few columns for variant inspection
+#'
+#' @param snv
+#'
+#' @return
+#' @export
+#'
+#' @examples
 display_variants = function(snv){
   snv = dplyr::select(snv, rowid, gene, transcript, coding, protein, AF)
   return(snv)
 }
 
 
+#' Select few columns for metrics inspection
+#'
+#' @param snv
+#'
+#' @return
+#' @export
+#'
+#' @examples
 display_std_metrics = function(snv){
   snv = dplyr::select(snv, rowid, gene, protein, AF, QUAL, totalDepth )
   return(snv)
